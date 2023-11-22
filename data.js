ee5a008591f1faccc3294f4a0220d4ded0aea8f5
axhellyn <145183042+axhellyn@users.noreply.github.com>
2023-11-22 21:38:54 +0700

    Upload Submission 1 FE

---
+++ data.js
@@ -0,0 +1,18 @@
+function redirectCard2(){
+   console.log('hai');
+   window.location.href="https://enhas.sayatmpan.repl.co/";
+}
+
+function redirectWhatsapp(){
+   console.log('hai');
+   window.location.href="https://wa.me/+6285377355623";
+}
+
+function redirectInstagram(){
+   console.log('hai');
+   window.location.href="https://www.instagram.com/dy.dhna_";
+}
+
+function redirectLinkedin(){
+   window.location.href = "http://www.linkedin.com/in/ra-nur-afifah-widyadhana-408393296";
+}
